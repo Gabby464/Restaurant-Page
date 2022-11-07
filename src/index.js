4bf34b12c3b1d1eafce437a406782ae564a546df
Gabriela P <gabriela.poryazova@gmail.com>
2022-11-07 21:13:46 +0200

    Add home and menu buttons' functionality

--- src/index.js
+++ src/index.js
@@ -5,5 +5,16 @@ import {
     createHomePage
 } from './homePage';
 import background from './bg.jpg';
+import Item from './menuPage';
+import {menuCreator} from './menuPage'
 const bodyContainer = renderTopPage(background);
 createHomePage(bodyContainer);
+
+function listeners (){
+    const menuBtn = document.getElementById('menu');
+    const homeBtn = document.getElementById('home');
+
+    menuBtn.addEventListener('click', function () {menuCreator(bodyContainer)});
+    homeBtn.addEventListener('click', function () {createHomePage(bodyContainer)});
+}
+listeners();
