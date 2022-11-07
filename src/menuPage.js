a282acc4181fe7605306e0b0b147ce625c42ea5a
Gabriela P <gabriela.poryazova@gmail.com>
2022-11-07 21:13:15 +0200

    Optimize element creation for the menu's content

--- src/menuPage.js
+++ src/menuPage.js
@@ -1,9 +1,9 @@
-export class Item{
+export class Item {
     constructor(Prname, price) {
         this.Prname = Prname;
         this.price = price;
     }
-    createEl(){
+    createEl() {
         const itemDiv = document.createElement("div");
         itemDiv.className = "menu-item";
 
@@ -11,14 +11,6 @@ export class Item{
         header.className = "name-item";
         header.textContent = this.Prname;
         itemDiv.appendChild(header);
-
-        const itemImage = document.createElement("div");
-        itemImage.className = "image-container";
-        const image = document.createElement("img");
-        itemImage.appendChild(image);
-        itemDiv.appendChild(itemImage);
-        image.src = `Menu-images/${this.Prname.toLowerCase()}.jpeg`;
-        image.alt = `${this.Prname}`;
 
         const price = document.createElement("div");
         price.className = "price";
@@ -31,30 +23,53 @@ export class Item{
 
 export const menuCreator = (bodyContainer) => {
     bodyContainer.innerHTML = ""
-    const main = document.createElement("div");
-    main.className = "main-menu";
-    const salads = document.createElement("div");
-    salads.className = "salads-menu";
-    const desserts = document.createElement("div");
-    desserts.className = "dessert-menu"
+        //create nodes and return section element;
+        class Section {
+            constructor(content) {
+                this.content = content
+            }
+            createElelment(content) {
+                const mainDiv = document.createElement("div");
+                mainDiv.className = 'menu-section';
+                //main div = header + sectionItems
+                const header = document.createElement("div");
+                header.textContent = content;
+                const sectionItems = document.createElement("div");
+                sectionItems.class = "section-items";
+                mainDiv.append(header, sectionItems);
+                bodyContainer.appendChild(mainDiv);
+                return sectionItems;
+            }
+        }
+        const createSections = () => {
+            const salads = new Section('Salads');
+            const saladItems = salads.createElelment();
+            const main = new Section('Main Courses');
+            const mainItems = main.createElelment();
+            const desserts = new Section('Desserts');
+            const dessertItems = desserts.createElelment()
+            return([saladItems, mainItems, dessertItems])
+        }
 
-    bodyContainer.append(salads, main, desserts)
+        ;
+        const fillInSections = () => {
+            const allSections = createSections()
+            let saladItems, mainItems, dessertItems;
+            [saladItems, mainItems, dessertItems] = allSections;
+            const burrito = new Item('Burrito', '14.99USD');
+            const pasta = new Item('Pasta', '18.99USD');
+            const club = new Item('Club Sandwich', '11.99USD');
+            const trufflePasta = new Item('Truffle Pasta', '21.99USD');
+            const steak = new Item('Steak', '22.99USD');
+            const cake = new Item('Cake', '7.99USD');
+            const pancakes = new Item('Pancakes', '11.29USD');
+            const salad = new Item('Salad', '9.99USD');
+            const greekSalad = new Item('Greek Salad', '10.99USD');
+        
+            mainItems.append(club.createEl(), burrito.createEl(), steak.createEl(), pasta.createEl(), trufflePasta.createEl());
+            saladItems.append(salad.createEl(), greekSalad.createEl());
+            dessertItems.append(cake.createEl(), pancakes.createEl())
+        }
 
-    const burrito = new Item('Burrito', '14.99USD');
-    const pasta = new Item('Pasta', '18.99USD');
-    const club = new Item('Club Sandwich','11.99USD');
-    const trufflePasta = new Item('Truffle Pasta', '21.99USD');
-    const steak = new Item('Steak', '22.99USD')
-
-    main.append(club.createEl(), burrito.createEl(), steak.createEl(), pasta.createEl(), trufflePasta.createEl());
-
-    const salad = new Item('Salad', '9.99USD');
-    const greekSalad = new Item('Greek Salad', '10.99USD');
-    
-    salads.append(salad.createEl(), greekSalad.createEl());
-
-    const cake = new Item('Cake', '7.99USD');
-    const pancakes = new Item('Pancakes', '11.29USD')
-
-    desserts.append(cake.createEl(), pancakes.createEl())
+        fillInSections()
 }
