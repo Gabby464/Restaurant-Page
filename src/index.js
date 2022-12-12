abf3fa578e8140b810f558fc7ef52ed7182c4a15
Gabriela P <gabriela.poryazova@gmail.com>
2022-12-12 22:39:36 +0200

    Create contact Page content

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import {
     createHomePage
 } from './homePage';
 import background from './bg.jpg';
-import Item from './menuPage';
 import {menuCreator} from './menuPage';
 import image1 from './slideshow/1.jpg';
 import image2 from './slideshow/2.jpg';
 import image3 from './slideshow/3.jpg';
 import image4 from './slideshow/4.jpg';
 export const images = [image1, image2, image3, image4];
-import  {createGalleryElements, createSlides, showSlides, loadslides, plusSlides} from './galleryPage'
+import  {createGalleryElements, createSlides, showSlides, loadslides, plusSlides} from './galleryPage';
+import {createMainLayout} from './contactPage';
 
 // require.context('./slideshow', true, /.jpg$/);
 const bodyContainer = renderTopPage(background);
@@ -22,9 +22,11 @@ function listeners (){
     const menuBtn = document.getElementById('menu');
     const homeBtn = document.getElementById('home');
     const galleryBtn = document.getElementById('galery');
+    const contactBtn = document.getElementById('contact');
     menuBtn.addEventListener('click', function () {menuCreator(bodyContainer)});
     homeBtn.addEventListener('click', function () {createHomePage(bodyContainer)});
     galleryBtn.addEventListener('click', function () {renderSlideshow(bodyContainer)})
+    contactBtn.addEventListener('click', function () {createMainLayout(bodyContainer)})
 }
 listeners();
 
