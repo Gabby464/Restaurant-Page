4c90f10afeb6889d485b1bf8650caa5cd912c33d
Gabriela P <gabriela.poryazova@gmail.com>
2022-12-12 20:56:42 +0200

    Create simple gallery rendering with 4 images

--- src/galleryPage.js
+++ src/galleryPage.js
@@ -1,1 +1,48 @@
-import images from './index.js';
+//with the help ofhttps://www.w3schools.com/howto/howto_js_slideshow_gallery.asp
+    const createGalleryElements = (bodyContainer) => {
+    bodyContainer.innerHTML = '';
+    const mainContainer = document.createElement('div');
+    mainContainer.className = 'main-gallery'
+    bodyContainer.appendChild(mainContainer);
+
+    //  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    //  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    const prevButton = document.createElement('prev');
+    prevButton.className = 'prev'
+    prevButton.innerHTML ='&#10094;';
+    prevButton.addEventListener('click', function() {plusSlides(1)});
+    const nextButton = document.createElement('next');
+    nextButton.className = 'next'
+    nextButton.innerHTML ='&#10095;';
+    nextButton.addEventListener('click', function() {plusSlides(-1)});
+    mainContainer.append(prevButton, nextButton);
+}
+    function createSlides(path, bodyContainer) {
+  const galeryContainer = document.createElement('div');
+  galeryContainer.className = 'gallery-container';
+  const imageEl = document.createElement('img');
+  galeryContainer.appendChild(imageEl);
+  imageEl.src = path;
+  imageEl.style = 'width:100%';
+  bodyContainer.appendChild(galeryContainer);
+}
+    let slideIndex = 1;
+    function loadslides (){
+    showSlides(slideIndex);
+    }
+    // Next/previous controls
+    function plusSlides(index) {
+      showSlides(slideIndex += index);
+    }
+    
+    function showSlides(n) {
+      let slides = document.getElementsByClassName('gallery-container');
+      if (n > slides.length) {slideIndex = 1} // go back to the first
+      if (n < 1) {slideIndex = slides.length} // go to the last
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      console.log(slides)
+      slides[slideIndex-1].style.display = "block";
+}
+export {createGalleryElements, createSlides, showSlides, loadslides, plusSlides, slideIndex}
--- src/index.js
+++ src/index.js
@@ -11,7 +11,9 @@ import image1 from './slideshow/1.jpg';
 import image2 from './slideshow/2.jpg';
 import image3 from './slideshow/3.jpg';
 import image4 from './slideshow/4.jpg';
-export const images = [image1, image2, image3, image4]
+export const images = [image1, image2, image3, image4];
+import  {createGalleryElements, createSlides, showSlides, loadslides, plusSlides} from './galleryPage'
+
 // require.context('./slideshow', true, /.jpg$/);
 const bodyContainer = renderTopPage(background);
 createHomePage(bodyContainer);
@@ -19,9 +21,18 @@ createHomePage(bodyContainer);
 function listeners (){
     const menuBtn = document.getElementById('menu');
     const homeBtn = document.getElementById('home');
-    const galleryBtn = document.getElementById('gallery');
-
+    const galleryBtn = document.getElementById('galery');
     menuBtn.addEventListener('click', function () {menuCreator(bodyContainer)});
     homeBtn.addEventListener('click', function () {createHomePage(bodyContainer)});
+    galleryBtn.addEventListener('click', function () {renderSlideshow(bodyContainer)})
 }
 listeners();
+
+function renderSlideshow(bodyContainer) {
+    createGalleryElements(bodyContainer);
+    createSlides(image1, bodyContainer);
+    createSlides(image2, bodyContainer);
+    createSlides(image3, bodyContainer);
+    createSlides(image4, bodyContainer);
+    loadslides(plusSlides, showSlides)
+}
